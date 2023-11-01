f0d9dc26848ef02de4ca1b679760063e82007f9b
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2023-11-01 00:23:33 +0000

    Added javadocs for Activity class

--- src/main/java/org/example/Activity.java
+++ src/main/java/org/example/Activity.java
@@ -1,20 +1,10 @@
 package org.example;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Activity implements Comparable<Activity> {
 
-  /**  public static void main(String[] args) throws ParseException {
-        String dateString = "10/10/2022";
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-        Date date = dateFormat.parse(dateString);
-
-        Activity a = new Activity(ActivityType.valueOf("RUNNING"), date, 17, 125, 20);
-        System.out.println(a);
-    }
-   */
     private ActivityType activityType;
 
     private Date date;
@@ -24,147 +14,222 @@ public class Activity implements Comparable<Activity> {
     private double caloriesBurned;
     private double distance;
 
+    /**
+     * Instantiates a new Activity.
+     */
 
     public Activity() {
     }
 
 
-
-     public Activity(ActivityType activityType, Date date, double duration, double aveHeartRate, double distance) {
+    /**
+     * Instantiates a new Activity.
+     *
+     * @param activityType the activity type
+     * @param date         the date
+     * @param duration     the duration
+     * @param aveHeartRate the ave heart rate
+     * @param distance     the distance
+     */
+    public Activity(ActivityType activityType, Date date, double duration, double aveHeartRate, double distance) {
             this.activityType = activityType;
             this.date = date;
             this.duration = duration;
             this.aveHeartRate = aveHeartRate;
             this.distance = distance;
-         this.intensity = calcIntensity(); // Set the intensity based on the provided data
-         this.caloriesBurned = calcCaloriesBurned(); // Calculate calories burned based on intensity
+            this.intensity = calcIntensity(); // Set the intensity based on the provided data
+            this.caloriesBurned = calcCaloriesBurned(); // Calculate calories burned based on intensity
         }
 
 
+    /**
+     * Gets intensity.
+     * @return the intensity
+     */
     public String getIntensity() {
         return intensity;
     }
 
+    /**
+     * Gets calories burned.
+     * @return the calories burned
+     */
     public double getCaloriesBurned() {
         return caloriesBurned;
     }
 
+    /**
+     * Gets activity type.
+     * @return the activity type
+     */
     public ActivityType getActivityType() {
         return activityType;
     }
 
+    /**
+     * Sets activity type.
+     * @param activityType the activity type
+     */
     public void setActivityType(ActivityType activityType) {
         this.activityType = activityType;
     }
 
+    /**
+     * Gets date.
+     * @return the date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Sets date.
+     * @param date the date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Gets duration.
+     * @return the duration
+     */
     public double getDuration() {
         return duration;
     }
 
+    /**
+     * Sets duration.
+     * @param duration the duration
+     */
     public void setDuration(double duration) {
         this.duration = duration;
     }
 
+    /**
+     * Gets ave heart rate.
+     * @return the ave heart rate
+     */
     public double getAveHeartRate() {
         return aveHeartRate;
     }
 
+    /**
+     * Sets ave heart rate.
+     * @param aveHeartRate the ave heart rate
+     */
     public void setAveHeartRate(double aveHeartRate) {
         this.aveHeartRate = aveHeartRate;
     }
 
 
-
+    /**
+     * Sets intensity.
+     * @param intensity the intensity
+     */
     public void setIntensity(String intensity) {
         this.intensity = intensity;
     }
 
 
-
+    /**
+     * Sets calories burned.
+     * @param caloriesBurned the calories burned
+     */
     public void setCaloriesBurned(double caloriesBurned) {
         this.caloriesBurned = caloriesBurned;
     }
+
+    /**
+     * Calc intensity.
+     * Calculates the intensity of an activity based on the distance and duration.
+     * The intensity is categorized as Very Light, Light, Moderate, Vigorous, or Very Vigorous.
+     * @return the intensity as a string or "null" if the activity type is not recognized.
+     */
     public String calcIntensity() {
+        //get the speed in kilometers per minute
+        String intensity = null;
         double km = this.distance / this.duration * 60;
+        // Check to see the activity type to determine intensity
         if (this.activityType == ActivityType.SWIMMING) {
             if (km < 0.5) {
-                return "Very Light";
+                intensity = "Very Light";
             } else if (km > 0.5 && km <= 1.25) {
-                return "Light";
+                intensity = "Light";
             } else if (km > 1.25 && km <= 2) {
-                return "Moderate";
+                intensity = "Moderate";
             } else if (km > 2 && km <= 2.75) {
-                return "Vigorous";
+                intensity = "Vigorous";
             } else if (km > 2.75 && km <= 3.5) {
-                return "Very Vigorous";
+                intensity = "Very Vigorous";
             }
         }
         if (this.activityType == ActivityType.RUNNING) {
             if (km <= 4) {
-                return "Very Light";
+                intensity = "Very Light";
             } else if (km > 4 && km <= 8) {
-                return "Light";
+                intensity = "Light";
             } else if (km > 8 && km <= 12) {
-                return "Moderate";
+                intensity = "Moderate";
             } else if (km > 12 && km <= 16) {
-                return "Vigorous";
+                intensity = "Vigorous";
             } else if (km > 16 && km <= 24) {
-                return "Very Vigorous";
+                intensity = "Very Vigorous";
             }
         }
         if (this.activityType == ActivityType.CYCLING) {
             if (km <= 8) {
-                return "Very Light";
+                intensity = "Very Light";
             } else if (km > 8 && km <= 16) {
-                return "Light";
+                intensity = "Light";
             } else if (km > 16 && km <= 25) {
-                return "Moderate";
+                intensity = "Moderate";
             } else if (km > 25 && km <= 33) {
-                return "Vigorous";
+                intensity = "Vigorous";
             } else if (km > 33 && km <= 40) {
-                return "Very Vigorous";
+                intensity = "Very Vigorous";
             }
         }
-        return "null";
+        //return the intensity
+        return intensity;
     }
 
-    // Calories burned based on energy expended, type of activity and duration
+    /**
+     * Calculates the calories burned during an activity based on its intensity and duration.
+     * @return The total calories burned as a double value
+     */
     public double calcCaloriesBurned() {
-        double intensity_value = 0.0;
-
-        if (intensity.equals("Very Light")) {
-            intensity_value = 2;
-        } else if (intensity.equals("Light")) {
-            intensity_value = 5;
-        } else if (intensity.equals("Moderate")) {
-            intensity_value = 7;
-        } else if (intensity.equals("Vigorous")) {
-            intensity_value = 13;
-        } else if (intensity.equals("Very Vigorous")) {
-            intensity_value = 15;
+        double calories = 0.0;
+        // Check the intensity to determine the calorie burn rate
+        if (intensity != null) {
+            if (intensity.equals("Very Light")) {
+                calories = 2;
+            } else if (intensity.equals("Light")) {
+                calories = 5;
+            } else if (intensity.equals("Moderate")) {
+                calories = 7;
+            } else if (intensity.equals("Vigorous")) {
+                calories = 13;
+            } else if (intensity.equals("Very Vigorous")) {
+                calories = 15;
+            }
         }
-
-        return intensity_value * this.duration;
+        //get the calories burned by multiplying the calorie burn rate by the duration
+        return calories * this.duration;
     }
 
-    //Energy expended based on the average kilometres per hour
-   // public double calcEnergyExpended(){
-
-     //return 0.0;
-  //}
-
+    /**
+     * Gets distance.
+     * @return the distance
+     */
     public double getDistance() {
         return distance;
     }
 
+    /**
+     * Sets distance.
+     * @param distance the distance
+     */
     public void setDistance(double distance) {
         this.distance = distance;
     }
@@ -183,8 +248,12 @@ public class Activity implements Comparable<Activity> {
                 '}';
     }
 
-
-
+    /**
+     * Compares this Activity to another Activity based on their dates
+     *@param o The Activity object to compare to.
+     *@return A negative integer, zero, or a positive integer if this Activity's date is
+     * less than, equal to, or greater than the date of the provided Activity.
+     */
     @Override
     public int compareTo(Activity o) {
         return this.date.compareTo(o.date);
